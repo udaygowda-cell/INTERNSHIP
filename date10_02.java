5dd48645a2f77fc9a2d7631e8fc2ad6fb6a215ce
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-10 22:54:42 +0530

    bnot

--- date10_02.java
+++ date10_02.java
@@ -1,14 +1,14 @@
 //left shift operator
-public class date10_02 {
-    public static void main(String[] args) {
-        int a=10;
-        System.out.println(a<<1);
-        System.out.println(a<<2);
-        System.out.println(a<<3);
-        System.out.println(a<<4);
-        System.out.println(a>>1);
+// public class date10_02 {
+//     public static void main(String[] args) {
+//         int a=10;
+//         System.out.println(a<<1);
+//         System.out.println(a<<2);
+//         System.out.println(a<<3);
+//         System.out.println(a<<4);
+//         System.out.println(a>>1);
         
-    }}
+//     }}
 
 //Bitwise operators
 // public class date10_02 {
@@ -22,3 +22,12 @@ public class date10_02 {
 //         System.out.println(a^b);
 
 //     }}
+//bit wise not
+public class date10_02 {
+    public static void main(String[] args) {
+        int a = 21;
+        int b = 27;
+        System.out.println(~a);//bitwise not
+        System.out.println(~b);
+    }}
+
