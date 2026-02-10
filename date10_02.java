e006f04f91bee691010821d201900809741940da
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-10 22:25:52 +0530

    bitwise and operator

--- date10_02.java
+++ date10_02.java
@@ -1,5 +1,15 @@
+//left shift operator
+// public class date10_02 {
+//     public static void main(String[] args) {
+//         int a=10;
+//         System.out.println(a<<2);
+//     }}
+
+//operator precedence
 public class date10_02 {
     public static void main(String[] args) {
-        int a=10;
-        System.out.println(a<<2);
+        int a = 21;
+        int b = 27;
+        System.out.println(a&b);
+
     }}
