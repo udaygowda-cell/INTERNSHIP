ab0c63b7f38fd582c461436bfe2edae99dd3d81d
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-10 22:28:21 +0530

    bitwise or operator

--- date10_02.java
+++ date10_02.java
@@ -10,6 +10,8 @@ public class date10_02 {
     public static void main(String[] args) {
         int a = 21;
         int b = 27;
-        System.out.println(a&b);
+        System.out.println(a&b);//bitwise and
+        //bitwise or
+        System.out.println(a|b);
 
     }}
