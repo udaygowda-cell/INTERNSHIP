7c4c18b796fda725a1a031c5f7b8ad7a4f9147b1
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-10 22:32:23 +0530

    left shift

--- date10_02.java
+++ date10_02.java
@@ -1,19 +1,23 @@
 //left shift operator
-// public class date10_02 {
-//     public static void main(String[] args) {
-//         int a=10;
-//         System.out.println(a<<2);
-//     }}
-
-//operator precedence
 public class date10_02 {
     public static void main(String[] args) {
-        int a = 21;
-        int b = 27;
-        System.out.println(a&b);//bitwise and
-        //bitwise or
-        System.out.println(a|b);
-        //bitwise xor
-        System.out.println(a^b);
+        int a=10;
+        System.out.println(a<<1);
+        System.out.println(a<<2);
+        System.out.println(a<<3);
+        
 
     }}
+
+//Bitwise operators
+// public class date10_02 {
+//     public static void main(String[] args) {
+//         int a = 21;
+//         int b = 27;
+//         System.out.println(a&b);//bitwise and
+//         //bitwise or
+//         System.out.println(a|b);
+//         //bitwise xor
+//         System.out.println(a^b);
+
+//     }}
