0ae7eed5a9d6f3a685f87aec4a889d8c5d42e44e
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-11 11:32:15 +0530

    pre operation

--- date_11_02.java
+++ date_11_02.java
@@ -23,26 +23,85 @@
 
 
 // untary operATORS
-public class date_11_02{
-    public static void main(String[] args){
+//post operators
+// public class date_11_02{
+//     public static void main(String[] args){
         // int a = 5;
         // System.out.println("befor " + a++);
         // System.out.println(a);
       
         
         // int b =9;
-        // System.out.println(b--);
+        // System.out.println(b--);//9 gose left to right so first comes b it print decremented value then decrement it
         // System.out.println(b);
 
-        int x = 17;
-        System.out.println(x++);//17
-        System.out.println(x++);//18
-        System.out.println(x);//19
+        // int x = 17;
+        // System.out.println(x++);//17
+        // System.out.println(x++);//18
+        // System.out.println(x);//19
 
-        int y = 14;
-        System.out.println(y--);//14
-        System.out.println(y--);//13
-        System.out.println(y);//12
+        // int y = 14;
+        // System.out.println(y--);//14
+        // System.out.println(y--);//13
+        // System.out.println(y);//12
+
+        // int a = 11;
+        // int b = a++ + a++;//11+12
+        // System.out.println(b);
+
+        // int c = 15;
+        // int d = c-- + c--;//15+14
+        // System.out.println(d);
+
+//         int p = 21;
+//         int q = p++ + p++ + p++;//21+22+23
+//         System.out.println(q);
+
+//         int r =27;
+//         int s = r-- + r-- + r--;//27+26+25
+//         System.out.println(s);
+
+//     }
+// }
+
+
+// pre operation 
+public class date_11_02{
+    public static void main(String[] args){
+        // int a = 4;
+        // System.out.println("before " + ++a);//5 first increment then print
+        // System.out.println(a);//5
+      
+        
+        // int b =9;
+        // System.out.println(--b);//8 gose left to right so first comes b it print decremented value then decrement it
+        // System.out.println(b);//8
+
+        int x = 13;
+        System.out.println(++x);//14 first increment then print
+        System.out.println(++x);//15 first increment then print
+        System.out.println("x" + x);//15
+
+        int y = 19  ;
+        System.out.println(--y);//18 first decrement then print
+        System.out.println(--y);//17 first decrement then print
+        System.out.println("y " +y);//12
+
+        int a1 = 11;
+        int b1 = ++a1 + ++a1;//12+13
+        System.out.println(b1);
+
+        int c = 15;
+        int d = --c + --c;//14+13
+        System.out.println(d);
+
+        int p = 21;
+        int q = ++p + ++p + ++p;//22+23+24
+        System.out.println(q);
+
+        int r =27;
+        int s = --r + --r + --r;//26+25+24
+        System.out.println(s);
 
     }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+   // int a = 11;
+        // int b = a++ + a++;//11+12
+        // System.out.println(b);
+
+        // int c = 15;
+        // int d = c-- + c--;//15+14
+        // System.out.println(d);
