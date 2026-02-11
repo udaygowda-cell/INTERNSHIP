eeb5b8550fec5880993d21f39a84701c3220e235
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-11 11:05:42 +0530

    post operator

--- date_11_02.java
+++ date_11_02.java
@@ -1,20 +1,38 @@
+// public class date_11_02{
+//     public static void main(String[] args){
+//     //     int a = 9;
+//     //  System.out.println(a>>1);
+//     //  int b = 25;
+//     //  System.out.println(b>>1);
+//     //  int c = 13;
+//     //  System.out.println(c>>1);
+
+//     int p=45;
+//     System.out.println(p>>2);
+//     int q= 27;
+//     System.out.println(q>>2);   
+//     int r = 75;
+//     System.out.println(r>>3);
+//     int s= 143;
+//     System.out.println(s>>3);
+    
+// }
+// }
+
+
+
+
+// untary operATORS
 public class date_11_02{
     public static void main(String[] args){
-    //     int a = 9;
-    //  System.out.println(a>>1);
-    //  int b = 25;
-    //  System.out.println(b>>1);
-    //  int c = 13;
-    //  System.out.println(c>>1);
+        int a = 5;
+        System.out.println("befor " + a++);
+        System.out.println(a);
+      
+        
+        int b =9;
+        System.out.println(b--);
+        System.out.println(b);
 
-    int p=45;
-    System.out.println(p>>2);
-    int q= 27;
-    System.out.println(q>>2);   
-    int r = 75;
-    System.out.println(r>>3);
-    int s= 143;
-    System.out.println(s>>3);
-    
-}
+    }
 }
