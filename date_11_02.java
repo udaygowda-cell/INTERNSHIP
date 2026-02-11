e57ca3b5300d0e84e9f9072d334459a08e076c0d
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-11 10:48:02 +0530

    right shift

---
+++ date_11_02.java
@@ -0,0 +1,11 @@
+public class date_11_02{
+    public static void main(String[] args){
+        int a = 9;
+     System.out.println(a>>1);
+     int b = 25;
+     System.out.println(b>>1);
+     int c = 13;
+     System.out.println(c>>1);
+    
+}
+}
