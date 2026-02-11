28c05d11f9f55e194cb24df398f5fba2f8a63b07
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-11 11:00:40 +0530

    right sshift 3

--- date_11_02.java
+++ date_11_02.java
@@ -1,11 +1,20 @@
 public class date_11_02{
     public static void main(String[] args){
-        int a = 9;
-     System.out.println(a>>1);
-     int b = 25;
-     System.out.println(b>>1);
-     int c = 13;
-     System.out.println(c>>1);
+    //     int a = 9;
+    //  System.out.println(a>>1);
+    //  int b = 25;
+    //  System.out.println(b>>1);
+    //  int c = 13;
+    //  System.out.println(c>>1);
+
+    int p=45;
+    System.out.println(p>>2);
+    int q= 27;
+    System.out.println(q>>2);   
+    int r = 75;
+    System.out.println(r>>3);
+    int s= 143;
+    System.out.println(s>>3);
     
 }
 }
