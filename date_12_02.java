b9e2ed40bafc1dd66d083ca2856211535af4ff4b
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-12 10:39:59 +0530

    new topicl

--- date_12_02.java
+++ date_12_02.java
@@ -1,6 +1,15 @@
 public class date_12_02 {
     public static void main(String[] args){
-        
+        for(int i=0;i<=10;i++){
+            if(i%2==0){
+                System.out.println(i+" even");
+            }
+            else{
+                System.out.println(i+ " odd");
+            }
+
+        }
+
     }
     
 }
