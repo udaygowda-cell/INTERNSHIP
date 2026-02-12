f4e4157045074e980b46be18010d45c518c1ad8f
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-12 11:35:32 +0530

    else if

--- date_12_02.java
+++ date_12_02.java
@@ -31,63 +31,92 @@
 
 
 //IF ELSE IF LADDER
+import java.util.Scanner;
+// public class date_12_02 {
+//     public static void main(String[] args){  
+//     //    if (false){
+//     //     System.out.println("developer");
+//     //      }else{
+//     //         System.out.println("tester");
+//     //      }
+
+
+//     //even or odd
+//     // int n = 15;
+//     // if (n%2==0){
+//     //     System.out.println(n+" even"); 
+//     // }else{
+//     //     System.out.println(n+ " odd");
+//     // }  
+
+//     // int age =2;
+//     // if (age>=18){
+//     //     System.out.println("major");
+//     // }
+//     // else{
+//     //     System.out.println("minor");
+//     // }
+
+//     Scanner sc = new Scanner(System.in);
+
+//     // double accbalance = 10000.00;
+//     // System.out.println("enter the amount to withdraw");
+//     // int withdraw = sc.nextInt();
+//     // if (withdraw<=accbalance){
+//     //     System.out.println("withdraw successful");
+//     // }else{
+//     //     System.out.println("insufficient balance");
+//     // }
+
+// }                  
+// }
+
+
+// IF ELSE IF LADDER
 public class date_12_02 {
     public static void main(String[] args){  
-    //    if (false){
-    //     System.out.println("developer");
-    //      }else{
-    //         System.out.println("tester");
-    //      }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a value");
+        int a = sc.nextInt();
+
+        
+        // if (a<7){
+        //     System.out.println("java full stack");
+        // }
+        // else if (a==7){
+        //     System.out.println("python full stack");
+        // }
+        // else{
+        //     System.out.println("data science");
+        // }
+        
 
 
-    //even or odd
-    // int n = 15;
-    // if (n%2==0){
-    //     System.out.println(n+" even"); 
-    // }else{
-    //     System.out.println(n+ " odd");
-    // }  
 
-    int age =2;
-    if (age>=18){
-        System.out.println("major");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
     }
-    else{
-        System.out.println("minor");
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}                  
-}
