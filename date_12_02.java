91c4ccb033b105d8038ca12acad4fcceb99a8cd9
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-12 10:55:51 +0530

    if

--- date_12_02.java
+++ date_12_02.java
@@ -1,15 +1,17 @@
+// 12.02.2024
+// IF STATEMENT
 public class date_12_02 {
     public static void main(String[] args){
-        for(int i=0;i<=10;i++){
-            if(i%2==0){
-                System.out.println(i+" even");
-            }
-            else{
-                System.out.println(i+ " odd");
-            }
+      
+        // System.out.println("program starts");
+        // if (true){
+        //     System.out.println("if block");
+        // }
+        //  System.out.println("program ends");
 
+
+        
         }
-
     }
     
-}
+
