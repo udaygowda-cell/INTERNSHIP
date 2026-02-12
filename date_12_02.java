47ba1db2e7a27826700e3da6fd604297a989c0d3
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-12 11:14:14 +0530

    if else

--- date_12_02.java
+++ date_12_02.java
@@ -1,28 +1,42 @@
 // 12.02.2024
 // IF STATEMENT
-public class date_12_02 {
-    public static void main(String[] args){
+// public class date_12_02 {
+//     public static void main(String[] args){
       
-        // System.out.println("program starts");
-        // if (true){
-        //     System.out.println("if block");
-        // }
-        //  System.out.println("program ends");
+//         // System.out.println("program starts");
+//         // if (true){
+//         //     System.out.println("if block");
+//         // }
+//         //  System.out.println("program ends");
 
 
 
-        // int a=4;
-        // if (a>5 && a<10){// two conditions are there both should be true
-        //     System.out.println("Bangalore");
+//         // int a=4;
+//         // if (a>5 && a<10){// two conditions are there both should be true
+//         //     System.out.println("Bangalore");
 
-        // }else{
-        //     System.out.println("Hyderabad");
+//         // }else{
+//         //     System.out.println("Hyderabad");
     
 
-        int x = 13;
-        if(x%2==0 || x%3==0){//if x is divisible by 2 or 3 on should be true
-            System.out.println("biriyani");
-    }
-}
+//         int x = 13;
+//         if(x%2==0 || x%3==0){//if x is divisible by 2 or 3 on should be true
+//             System.out.println("biriyani");
+//     }
+// }
 
-}
+// }
+
+
+
+
+//IF ELSE IF LADDER
+public class date_12_02 {
+    public static void main(String[] args){  
+       if (false){
+        System.out.println("developer");
+         }else{
+            System.out.println("tester");
+         }
+    }  
+}                  
