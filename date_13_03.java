95a342e08985fa1027770d933581ccae37e679bb
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-13 10:58:01 +0530

    example2

--- date_13_03.java
+++ date_13_03.java
@@ -22,26 +22,46 @@ public class date_13_03 {
 	
 // }
 
-double accbal=10000.0;
-int amt = 200;
-if(amt<accbal){
+// double accbal=10000.0;
+// int amt = 200;
+// if(amt<accbal){
 	
-	if(amt%100==0)
-	{
-		System.out.println("withdrawal success");
+// 	if(amt%100==0)
+// 	{
+// 		System.out.println("withdrawal success");
 		
-	}
-	else{
+// 	}
+// 	else{
 		
-		System.out.println("invalid Denomination");
-	}
+// 		System.out.println("invalid Denomination");
+// 	}
+// }
+// else{
+// 	System.out.println("Insufficinent Balance");
+// }
+
+
+char gender = 'F';
+int age = 22;
+if(gender=='M'){
+    if(age>21){
+        System.out.println("eligible bachelor");
+    }
+    else{
+        System.out.println("boy under age");
+    }
+}
+else if (gender=='F'){
+    if(age>18){
+        System.out.println("eligible spinster");
+    }
+    else{
+        System.out.println("girl under age");
+    }
 }
 else{
-	System.out.println("Insufficinent Balance");
+    System.out.println("invalid gender");
 }
-
-
-
 
 
 
