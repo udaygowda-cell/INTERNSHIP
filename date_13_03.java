da78c720d730f22c3e7041442324db2278139362
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-13 11:17:21 +0530

    using switch payment selection

--- date_13_03.java
+++ date_13_03.java
@@ -1,77 +1,105 @@
-public class date_13_03 {
-    public static void main(String[] args) {
-        
-//     int x = 17;
-// if(x%2==0)
-// {
-// 	if(x<10){
-// 		System.out.println("Pain poori");
-// 	}
-// 	else{
-// 		System.out.println("bhel poori");
-// 	}	
-// 	}
-// 	else {
-		
-// 		if (x<10){
-// 			System.out.println("masala poori");
-// 		}
-// 		else{
-// 		System.out.println("sev poori");
-// 	}
-	
-// }
+//NESTED IF STATEMENT
 
-// double accbal=10000.0;
-// int amt = 200;
-// if(amt<accbal){
+// public class date_13_03 {
+//     public static void main(String[] args) {
+        
+// //     int x = 17;
+// // if(x%2==0)
+// // {
+// // 	if(x<10){
+// // 		System.out.println("Pain poori");
+// // 	}
+// // 	else{
+// // 		System.out.println("bhel poori");
+// // 	}	
+// // 	}
+// // 	else {
+		
+// // 		if (x<10){
+// // 			System.out.println("masala poori");
+// // 		}
+// // 		else{
+// // 		System.out.println("sev poori");
+// // 	}
 	
-// 	if(amt%100==0)
-// 	{
-// 		System.out.println("withdrawal success");
+// // }
+
+// // double accbal=10000.0;
+// // int amt = 200;
+// // if(amt<accbal){
+	
+// // 	if(amt%100==0)
+// // 	{
+// // 		System.out.println("withdrawal success");
 		
-// 	}
-// 	else{
+// // 	}
+// // 	else{
 		
-// 		System.out.println("invalid Denomination");
-// 	}
+// // 		System.out.println("invalid Denomination");
+// // 	}
+// // }
+// // else{
+// // 	System.out.println("Insufficinent Balance");
+// // }
+
+
+// char gender = 'F';
+// int age = 22;
+// if(gender=='M'){
+//     if(age>21){
+//         System.out.println("eligible bachelor");
+//     }
+//     else{
+//         System.out.println("boy under age");
+//     }
+// }
+// else if (gender=='F'){
+//     if(age>18){
+//         System.out.println("eligible spinster");
+//     }
+//     else{
+//         System.out.println("girl under age");
+//     }
 // }
 // else{
-// 	System.out.println("Insufficinent Balance");
+//     System.out.println("invalid gender");
+// }
+// }
 // }
 
 
-char gender = 'F';
-int age = 22;
-if(gender=='M'){
-    if(age>21){
-        System.out.println("eligible bachelor");
-    }
-    else{
-        System.out.println("boy under age");
-    }
-}
-else if (gender=='F'){
-    if(age>18){
-        System.out.println("eligible spinster");
-    }
-    else{
-        System.out.println("girl under age");
-    }
-}
-else{
-    System.out.println("invalid gender");
-}
+
+
+
+
+//Switch statement
+
+
+public class date_13_03 {
+    public static void main(String[] args) {
+   int choice = 55;
+   switch (choice)
+   {
+    case 1: System.out.println("COD");
+    break;
+
+    case 2: System.out.println("UPI");
+    break;
+
+    case 3: System.out.println("CARD");
+    break;
+
+    case 4 : System.out.println("net banking");
+    break;
+     
+    default : System.out.println("Invalid Selecton");
+
+   }
 
 
 
 
 
 
-
-
-
-
-}
-}
-
+        
+    }}
