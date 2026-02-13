8a699d70881e2a4a13797d49869aac9a48ba9ae3
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-13 10:52:24 +0530

    example

--- date_13_03.java
+++ date_13_03.java
@@ -1,26 +1,57 @@
 public class date_13_03 {
     public static void main(String[] args) {
         
-    int x = 17;
-if(x%2==0)
-{
-	if(x<10){
-		System.out.println("Pain poori");
+//     int x = 17;
+// if(x%2==0)
+// {
+// 	if(x<10){
+// 		System.out.println("Pain poori");
+// 	}
+// 	else{
+// 		System.out.println("bhel poori");
+// 	}	
+// 	}
+// 	else {
+		
+// 		if (x<10){
+// 			System.out.println("masala poori");
+// 		}
+// 		else{
+// 		System.out.println("sev poori");
+// 	}
+	
+// }
+
+double accbal=10000.0;
+int amt = 200;
+if(amt<accbal){
+	
+	if(amt%100==0)
+	{
+		System.out.println("withdrawal success");
+		
 	}
 	else{
-		System.out.println("bhel poori");
-	}	
-	}
-	else {
 		
-		if (x<10){
-			System.out.println("masala poori");
-		}
-		else{
-		System.out.println("sev poori");
+		System.out.println("invalid Denomination");
 	}
-	
 }
+else{
+	System.out.println("Insufficinent Balance");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 }
 
--- note/date13.java
+++ note/date13.java
@@ -1,24 +1,21 @@
 class date13{
 public static void main (String[] args){
-int x = 17;
-if(x%2==0)
-{
-	if(x<10){
-		System.out.println("Pain poori");
+double accbal=10000.0;
+int amt = 13;
+if(amt<accbal){
+	
+	if(amt%100==0)
+	{
+		System.out.println("withdrawal success");
+		
 	}
 	else{
-		System.out.println("bhel poori");
-	}	
-	}
-	else {
 		
-		if (x<10){
-			System.out.println("masala poori");
-		}
-		else{
-		System.out.println("sev poori");
+		System.out.println("invalid Denomination");
 	}
-	
+}
+else{
+	System.out.println("Insufficinent Balance");
 }
 }
 }
