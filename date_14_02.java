e55de06c56ba6ff07b83bd2899bcb68521e1c26c
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-14 11:00:21 +0530

    example

--- date_14_02.java
+++ date_14_02.java
@@ -1,14 +1,56 @@
 public class date_14_02 {
     public static void main(String[] args){
-        int n= 5;
-        for(int i= 0;i<n;i++){
-            if(i%2==0){
-            System.out.print(1+" ");
-            }
-            else{
-                System.out.print(0+" ");
-            }
-        }
+       // int n= 5;
+        // for(int i= 0;i<n;i++){
+        //     if(i%2==0){
+        //     System.out.print(1+" ");
+        //     }
+        //     else{
+        //         System.out.print(0+" ");
+        //     }
+        // }
+
+        // for(int i= 0;i<n;i++){
+        //     if(i%2==0){
+        //     System.out.print("a"+" ");
+        //     }
+        //     else{
+        //         System.out.print("b"+" ");
+        //     }
+        // }
+
+
+        //  int n =5;
+        //  int val = 1;
+        //  for (int i=0;i<n;i++){
+
+        // if(i%2==0){
+        //     System.out.print(val+" ");
+        //     val++;
+        // }
+        // else {
+        //     System.out.print("*"+ " ");
+        // }
+        //  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
     
 }
