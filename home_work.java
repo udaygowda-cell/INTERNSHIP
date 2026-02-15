a9405db9ef6354f258231ed8b329679717ec09ef
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-15 15:02:42 +0530

    new commmit

--- home_work.java
+++ home_work.java
@@ -35,21 +35,34 @@
 // }
 
 
+// public class home_work {
+//     public static void main(String[] args){
+//     int n = 5;
+//         int val =1;
+// for (int i =0;i<n;i++)
+// {
+//     System.out.print(val +" ");
+// if (i<n/2)
+//     { 
+//     val++;
+//     }
+//     else
+//     {
+//         val--;
+//     }
+// }
+//     }
+// }
+
 public class home_work {
     public static void main(String[] args){
-    int n = 5;
-        int val =1;
-for (int i =0;i<n;i++)
-{
-    System.out.print(val +" ");
-if (i<n/2)
-    { 
-    val++;
-    }
-    else
-    {
-        val--;
-    }
-}
+        int n = 5;
+        for(int i =0; i<n; i++ ){
+            for(int j = 0; j<n;j++){
+             System.out.print("*");
+
+            }
+            System.out.println();
+        }
     }
 }
