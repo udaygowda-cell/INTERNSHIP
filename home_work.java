dbd39dcea378a7e9f84224459f147aca16a9598c
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-15 15:08:38 +0530

    new commmit

--- home_work.java
+++ home_work.java
@@ -82,11 +82,24 @@
 //     }
 // }
 
+// public class home_work {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i =0; i<n; i++ ){
+//             for(int j = 0; j<n+1;j++){
+//              System.out.print("*");
+
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
 public class home_work {
     public static void main(String[] args){
         int n = 5;
         for(int i =0; i<n; i++ ){
-            for(int j = 0; j<n+1;j++){
+            for(int j = 0; j<i+1;j++){
              System.out.print("*");
 
             }
