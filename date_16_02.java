30889017d02981ba917a59481a22b8e7986f2b72
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-16 11:25:41 +0530

    b

--- date_16_02.java
+++ date_16_02.java
@@ -178,6 +178,7 @@
 //   * * *
 
 
+//A
 // public class date_16_02 {
 //     public static void main(String[] args){
 //         int n = 5;
@@ -196,3 +197,25 @@
         
 //     }
 // }
+
+
+//B
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||j==0||i==n/2||j==n-1||i==n-1){// first row i==0, first column j==0, middle row i==n/2, last column j==n-1, last row i==n-1
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+        
+//     }
+// }
+
+
