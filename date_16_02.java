67fa7bfa728b8ab698303da9124e820c90f9c7ee
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-16 10:53:08 +0530

    square

--- date_16_02.java
+++ date_16_02.java
@@ -1,9 +1,28 @@
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||j==0||i==n-1||j==n-1){// first row i==0, first column j==0, last row, last column
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
 public class date_16_02 {
     public static void main(String[] args){
-        int n = 5;
+        int n = 10;
         for(int i=0;i<n;i++){
             for( int j =0; j<n;j++){
-                if (i==0||j==0||i==n-1||j==n-1){
+                if (i==0||j==0||i==4||j==4){
                     System.out.print("*"+" ");
                 }
                 else{
