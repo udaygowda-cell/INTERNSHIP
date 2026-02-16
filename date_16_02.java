256e4b08acc402bd8d79c026ae1365c324ef46e8
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-16 11:23:39 +0530

    A

--- date_16_02.java
+++ date_16_02.java
@@ -71,21 +71,128 @@
 
 
 //diagonal
-public class date_16_02 {
-    public static void main(String[] args){
-        int n = 10;
-        for(int i=0;i<n;i++){
-            for( int j =0; j<n;j++){
-                if (i==j || i+j==n-1){// diagonal from top left to bottom right i==j, diagonal from top right to bottom left i+j==n-1
-                    System.out.print("*"+" ");
-                }
-                else{
-                    System.out.print(" "+" ");
-                }
-            }
-            System.out.println();
-        }
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 10;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==j || i+j==n-1){// diagonal from top left to bottom right i==j, diagonal from top right to bottom left i+j==n-1
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
 
         
-    }
-}
+//     }
+// }
+
+
+
+
+
+
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0|| i==n/2 || i==n-1 || j==0 ||j==n/2 || j==n-1){// first row i==0, middle row i==n/2, last row i==n-1, first column j==0, last column j==n-1
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+
+        
+//     }
+// }
+
+// * * * * * 
+// *   *   *
+// * * * * *
+// *   *   *
+// * * * * *
+
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 10;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||j==n||i==n-1||i+j==n-1||i==j){// first row i==0, first column j==0, diagonal from top right to bottom left i+j==n-1, diagonal from top left to bottom right i==j
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
+// * * * * * * * * * * 
+//   *             *
+//     *         *
+//       *     *
+//         * *
+//         * *
+//       *     *
+//     *         *
+//   *             *
+// * * * * * * * * * *
+
+
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==j||i+j==n-1){// first row i==0, middle row i==n/2, last row i==n-1, first column j==0, middle column j==n/2, last column j==n-1
+//                     System.out.print(" "+" ");
+//                 }
+//                 else{
+//                     System.out.print("*"+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
+//   * * *   
+// *   *   *
+// * *   * *
+// *   *   *
+//   * * *
+
+
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||j==0||i==n/2||j==n-1){// first row i==0, first column j==0, middle row i==n/2, last column j==n-1
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
