7b81467083f72e66be76b3e93510087df01a8183
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-16 11:04:26 +0530

    diagonal

--- date_16_02.java
+++ date_16_02.java
@@ -17,12 +17,66 @@
 //     }
 // }
 
+
+
+
+
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 10;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||j==0||i==4||j==4){
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
+
+
+
+
+
+
+
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                // System.out.print(j==n/2||i==n/2);// middle column j==n/2, middle row i==n/2
+//                 if (i==n/2 || j==n/2){// middle row i==n/2, middle column j==n/2
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
+
+
+
+//diagonal
 public class date_16_02 {
     public static void main(String[] args){
         int n = 10;
         for(int i=0;i<n;i++){
             for( int j =0; j<n;j++){
-                if (i==0||j==0||i==4||j==4){
+                if (i==j || i+j==n-1){// diagonal from top left to bottom right i==j, diagonal from top right to bottom left i+j==n-1
                     System.out.print("*"+" ");
                 }
                 else{
