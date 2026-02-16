38174fae924d0653b05941eca7b3cceed950ebef
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-16 11:30:48 +0530

    b

--- date_16_02.java
+++ date_16_02.java
@@ -219,3 +219,104 @@
 // }
 
 
+
+//C
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||j==0||i==n-1){// first row i==0, first column j==0, last row i==n-1
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         } 
+//     }
+// }
+
+
+//D
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||j==0||i==n-1||j==n-1){// first row i==0, first column j==0, last row i==n-1, last column j==n-1
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
+
+// //E
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||j==0||i==n/2||i==n-1){// first row i==0, first column j==0, middle row i==n/2, last row i==n-1
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
+
+//F
+// public class date_16_02 {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||j==0||i==n/2){// first row i==0, first column j==0, middle row i==n/2
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
+//G
+public class date_16_02 {
+    public static void main(String[] args){
+        int n = 5;
+        for(int i=0;i<n;i++){
+            for( int j =0; j<n;j++){
+                if (i==0||j==0||i==n-1||j==n-1||i==n/2){// first row i==0, first column j==0, last row i==n-1, last column j==n-1, middle row i==n/2
+                    System.out.print("*"+" ");
+                }
+                else{
+                    System.out.print(" "+" ");
+                }
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
