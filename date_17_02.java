ad980d5d171839216168c8ce395e5a705cc03166
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-17 10:34:45 +0530

    while loop

---
+++ date_17_02.java
@@ -0,0 +1,11 @@
+public class date_17_02 {
+    public static void main(String[] args){
+        int a=1;
+
+        while(a<=5){
+            System.out.println(a+ " java");
+            a++;
+        }
+    
+}
+}
--- home_work.java
+++ home_work.java
@@ -54,24 +54,124 @@
 //     }
 // }
 
- public class home_work {
-    public static void main(String[] args){
-    int n = 5;
-        int val = 10;
-for (int i =0;i<n;i++)
-{
-//    System.out.print("val ="+val +" ");
-System.out.print(i<n/2);
- System.out.println(" i= "+i +" "+"n/2= "+ n/2 );
+//  public class home_work {
+//     public static void main(String[] args){
+//     int n = 5;
+//         int val = 10;
+// for (int i =0;i<n;i++)
+// {
+// //    System.out.print("val ="+val +" ");
+// System.out.print(i<n/2);
+//  System.out.println(" i= "+i +" "+"n/2= "+ n/2 );
  
-if (i<n/2)
-    { 
-    val +=10;
-    }
-    else
-    {
-        val -=10;
-    }
-}
+// if (i<n/2)
+//     { 
+//     val +=10;
+//     }
+//     else
+//     {
+//         val -=10;
+//     }
+// }
+//     }
+// }
+
+
+//Y
+// public class home_work {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==j&&j<=n/2 || i+j==n-1&&j>=n/2||j==n/2&&i>=n/2){
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+
+//             }
+    
+// System.out.println();
+//         }
+// }
+// }
+
+//S
+// public class home_work {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||i==n-1||j==0&&i<=n/2||i==n/2||j==n-1&&i>=n/2){
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+
+//             }
+    
+// System.out.println();
+//         }
+// }
+// }
+
+
+//T 
+// public class home_work {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (i==0||j==n/2){
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+//            }
+    
+// System.out.println();
+//         }
+// }
+// }
+
+//M
+// public class home_work {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=0;i<n;i++){
+//             for( int j =0; j<n;j++){
+//                 if (j==0||j==n-1||i==j&&j<=n/2||i+j==n-1&&j>=n/2){//
+//                     System.out.print("*"+" ");
+//                 }
+//                 else{
+//                     System.out.print(" "+" ");
+//                 }
+
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+//I
+public class home_work {
+    public static void main(String[] args){
+        int n = 5;
+        for(int i=0;i<n;i++){
+            for( int j =0; j<n;j++){
+                if (i==0||i==n-1||j==n/2){
+                    System.out.print("*"+" ");
+                }
+                else{
+                    System.out.print(" "+" ");
+                }
+
+            }
+            System.out.println();
+        }
     }
 }
