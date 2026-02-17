f533a29bd5aaebc811c255cce008dbe7d408024a
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-17 11:51:53 +0530

    sdsd

--- date_17_02.java
+++ date_17_02.java
@@ -90,17 +90,30 @@
 // 1 3 6 10 15 21 28 36 45 55
 
 
+// 
+
 public class date_17_02 {
     public static void main(String[] args){
-        int a=1;
-        int b = 10;
-        int num = 4;
-
-        while(a<=b){
-            int sum = num * a;
-            System.out.println(num+" * "+a +" = "+sum + " ");  ;   
-            a++; 
+        double a=1;
+       for (int i = 2; i<=23; i++){
+            double sum= (a/i)*100;
+            System.out.println((a+"/"+i)+" "+sum + " ");
         }
+
+    }
+}
+
+public class date_17_02 {
+    public static void main(String[] args){
+        int fact =1;
+        int n =5;
+
+        while(n>=1){
            
+            fact = fact*n;
+                n--;    
+        }
+       
+        System.out.println(fact);   
 }
 }
