561b4e4e8a12a082be100605eac30c44de849eb0
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-18 11:28:28 +0530

    perfect or not perfect numbers

--- date_18_02.java
+++ date_18_02.java
@@ -45,21 +45,48 @@
 // }
 // }
 
+
+
+
+// public class date_18_02 { 
+//     public static void main(String[] args){
+//   int num =15;    
+//   int a =1;
+//   int sum=0;
+//   //int b = num/2;
+// while (a<=num/2){// a<=b
+//     if (num%a==0){
+//         sum = sum + a;
+//     }
+//     a++;
+        
+//     }  
+//        System.out.println("sum is "+ sum);//sum of factors of num  
+// }
+// }
+
+
+
+
 public class date_18_02 { 
     public static void main(String[] args){
-  int num =15;    
+  int num =28;    
   int a =1;
   int sum=0;
   //int b = num/2;
 while (a<=num/2){// a<=b
     if (num%a==0){
-        System.out.println(a);
         sum = sum + a;
-     
     }
     a++;
         
     }  
        System.out.println("sum is "+ sum);//sum of factors of num  
+         if (sum==num){
+          System.out.println(num+" is a perfect number");
+         }
+         else{
+          System.out.println(num+" is not a perfect number");
+         }
 }
 }
