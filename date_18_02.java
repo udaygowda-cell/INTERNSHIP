ffedf4c3db479f45d4375aad6eefde3e407b5132
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-18 10:38:49 +0530

    do while

--- date_18_02.java
+++ date_18_02.java
@@ -1,3 +1,15 @@
-public class date_18_02 {
-    
+//DO- WHILE LOOP
+
+public class date_18_02 { 
+    public static void main(String[] args){
+      
+  int a =1;
+
+  do{
+    System.out.println("java");
+    a++;
+  }
+    while(a<5);
+        
+    }    
 }
--- tempCodeRunnerFile.java
+++
@@ -1,4 +0,0 @@
-
-        int n = 5;
-        for (int i =0 ; i<n;i++){
-            for (int j =0;j<n;j++){
