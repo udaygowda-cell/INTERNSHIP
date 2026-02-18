be9d5e7b6d9aa75744b8ad222c49a66b538f0d49
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-18 10:47:27 +0530

    do while

--- date_18_02.java
+++ date_18_02.java
@@ -1,15 +1,30 @@
 //DO- WHILE LOOP
 
+// public class date_18_02 { 
+//     public static void main(String[] args){
+      
+//   int a =1;
+
+//   do{
+//     System.out.println("java");
+//     a++;
+//   }
+//     while(a<5);
+        
+//     }    
+// }
+
+
 public class date_18_02 { 
     public static void main(String[] args){
       
-  int a =1;
+  int a =5;
 
   do{
-    System.out.println("java");
-    a++;
+    System.out.println(a+ " java");
+    a--;
   }
-    while(a<5);
+    while(a>=1);
         
     }    
 }
