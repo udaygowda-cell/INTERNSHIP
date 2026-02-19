63c00311fdfe9c9f8562e29b53324948712a7519
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-19 11:26:33 +0530

    methods

--- date_19_02.java
+++ date_19_02.java
@@ -1,6 +1,16 @@
+//METHODS
 public class date_19_02 {
-    public static void main (String[] args){
-        System.out.println();
+     
+    static void test()// should be static only. not call from static to non static
+    {
+        System.out.println("Executing test ........");
+    }
+
+      public static void main (String[] args){
+        System.out.println("program started");
+        test(); // calling test method
+        System.out.println("program ended");
+        
               
     }
 }
