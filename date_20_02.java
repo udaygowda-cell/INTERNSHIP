433da9fe174082c950229db004fc15360fd8210c
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-20 10:52:35 +0530

    passing method

--- date_20_02.java
+++ date_20_02.java
@@ -1,3 +1,5 @@
+//PASSING VALUES
+/* 
 public class date_20_02 {
     
 
@@ -14,3 +16,19 @@ public class date_20_02 {
 //         ACTUAL ARGUMENTS
     }
 }
+    */
+
+
+
+public class date_20_02 {
+               
+    static void test(double a){
+        System.out.println("values : "+a);
+    }
+    public static void main(String[] args){
+        test(25.5);
+        test(32.53);
+        test(44.55);
+    }
+}
+
