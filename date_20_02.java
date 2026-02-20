00fc16e98a761a154c190a26670d76ea0289481b
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-20 10:59:23 +0530

    passing method

--- date_20_02.java
+++ date_20_02.java
@@ -20,15 +20,30 @@ public class date_20_02 {
 
 
 
-public class date_20_02 {
+// public class date_20_02 {
                
-    static void test(double a){
-        System.out.println("values : "+a);
+//     static void test(double a){
+//         System.out.println("values : "+a);
+//     }
+//     public static void main(String[] args){
+//         test(25.5);
+//         test(32.53);
+//         test(44.55);
+//     }
+// }
+
+
+public class date_20_02 {
+    static void run(boolean bool){
+        System.out.println("boolean value : " + bool);
+    }
+               
+    static void test(char ch){
+        System.out.println("charector : "+ch);
+        run(true);
     }
     public static void main(String[] args){
-        test(25.5);
-        test(32.53);
-        test(44.55);
+     test('u');
     }
 }
 
