580bdee4b2fdebd181784c545fb729584b95657a
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-21 10:50:44 +0530

    add two number

--- date_21_02.java
+++ date_21_02.java
@@ -1,17 +1,37 @@
 
 //verify
-public class date_21_02 {
-    static boolean verify(int a){
-        if (a%2==0){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-public static void main(String[] args){
-    System.out.println("status "+ verify(10));
-    System.out.println("status "+ verify(15));
-}
+// public class date_21_02 {
+//     static boolean verify(int a){
+//         if (a%2==0){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+// public static void main(String[] args){
+//     System.out.println("status "+ verify(10));
+//     System.out.println("status "+ verify(15));
+// }
     
+// }
+
+
+public class date_21_02 {
+
+static double addition(double a, double b){
+    double sum = a+b;
+    return sum;
 }
+public static void main(String[] args){
+    System.out.println(addition(10, 20));
+    System.out.println(addition(10.5, 2.5));
+    System.out.println(addition(1.0, 20));
+    System.out.println(addition(10, 22.30));
+}
+}
+
+// output: 30.0
+// 13.0
+// 21.0
+// 32.3
