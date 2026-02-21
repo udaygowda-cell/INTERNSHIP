cda6a3dfc7357a3ddd8125132d586b9ea6cf4289
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-21 11:02:21 +0530

    square

--- date_21_02.java
+++ date_21_02.java
@@ -17,21 +17,35 @@
 // }
 
 
+// public class date_21_02 {
+
+// static double addition(double a, double b){
+//     double sum = a+b;
+//     return sum;
+// }
+// public static void main(String[] args){
+//     System.out.println(addition(10, 20));
+//     System.out.println(addition(10.5, 2.5));
+//     System.out.println(addition(1.0, 20));
+//     System.out.println(addition(10, 22.30));
+// }
+// }
+
+// // output: 30.0
+// // 13.0
+// // 21.0
+// // 32.3l
+
 public class date_21_02 {
 
-static double addition(double a, double b){
-    double sum = a+b;
-    return sum;
+static int square(int n){
+    int sq = n*n;
+    return sq;
 }
 public static void main(String[] args){
-    System.out.println(addition(10, 20));
-    System.out.println(addition(10.5, 2.5));
-    System.out.println(addition(1.0, 20));
-    System.out.println(addition(10, 22.30));
-}
-}
 
-// output: 30.0
-// 13.0
-// 21.0
-// 32.3
+    for(int i=1;i<=5;i++){
+        System.out.println(i+" square is "+square(i));
+    }
+}
+}
