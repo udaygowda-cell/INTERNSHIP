519ac2a020a8c93ae1d6639ff48843699a4f7c21
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-23 11:10:46 +0530

    print even and odd using scanner

--- MODULE_1/date_23_02.java
+++ MODULE_1/date_23_02.java
@@ -1,13 +1,31 @@
 //Scanner
 
+// import java.util.Scanner;
+// public class date_23_02 {
+//     public static void main(String[] args)
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the numbers:");
+//         int a = sc.nextInt();
+//         System.out.println("value is :"+a);
+//     }
+    
+// }
+
+
 import java.util.Scanner;
 public class date_23_02 {
     public static void main(String[] args)
     {
-Scanner sc = new Scanner(System.in);
-System.out.println("enter the numbers:");
-int a = sc.nextInt();
-System.out.println("value is :"+a);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the numbers:");
+        int a = sc.nextInt();
+
+        if(a%2==0){
+            System.out.println(" Even number ");
+        }else{
+        System.out.println("Odd number");
+        }
     }
     
 }
