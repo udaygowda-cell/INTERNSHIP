4db3edd5c180f0df7bcb121b8c4fbe88d8897067
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-23 11:16:31 +0530

    adding two numbers using scanner

--- MODULE_1/date_23_02.java
+++ MODULE_1/date_23_02.java
@@ -13,19 +13,36 @@
 // }
 
 
+// import java.util.Scanner;
+// public class date_23_02 {
+//     public static void main(String[] args)
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the numbers:");
+//         int a = sc.nextInt();
+
+//         if(a%2==0){
+//             System.out.println(" Even number ");
+//         }else{
+//         System.out.println("Odd number");
+//         }
+//     }
+    
+// }
+
+
 import java.util.Scanner;
 public class date_23_02 {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the numbers:");
-        int a = sc.nextInt();
+        System.out.println("enter the numbers 1:");
+        double a = sc.nextDouble();
+        System.out.println("enter the numbers 2:");
+        double b = sc.nextDouble();
 
-        if(a%2==0){
-            System.out.println(" Even number ");
-        }else{
-        System.out.println("Odd number");
-        }
+       double sum = a+b;
+    System.out.println("Sum of two munber is :"+ sum);
     }
     
 }
