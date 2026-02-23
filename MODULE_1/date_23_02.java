8e8e2d9a21050e1286da6f0df6139d60528075bc
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-23 11:05:14 +0530

    Scanner

--- MODULE_1/date_23_02.java
+++ MODULE_1/date_23_02.java
@@ -1,3 +1,13 @@
+//Scanner
+
+import java.util.Scanner;
 public class date_23_02 {
+    public static void main(String[] args)
+    {
+Scanner sc = new Scanner(System.in);
+System.out.println("enter the numbers:");
+int a = sc.nextInt();
+System.out.println("value is :"+a);
+    }
     
 }
