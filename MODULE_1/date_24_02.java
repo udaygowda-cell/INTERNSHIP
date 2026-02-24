26081d9d1650dc4518cf948e308fa5f90ccb16da
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-24 11:31:04 +0530

    Array

--- MODULE_1/date_24_02.java
+++ MODULE_1/date_24_02.java
@@ -1,4 +1,16 @@
+//
+
 public class date_24_02 {
-    
-    
+   public static void main(String[] args){
+    int[] arr = new int[5];
+    arr[0]=14;
+    arr[1]=10;
+    arr[2]=13;
+    arr[3]=15;
+    arr[4]=12;
+
+
+    for()
+   } 
+
 }
