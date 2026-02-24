5980d1af14a9b8e0ccfbc191be95febb8ab081f7
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-24 11:38:37 +0530

    store using for loop

--- MODULE_1/date_24_02.java
+++ MODULE_1/date_24_02.java
@@ -1,18 +1,28 @@
-//
+//ARRAY
+
+// public class date_24_02 {
+//    public static void main(String[] args){
+//     int[] arr = new int[5];
+//     arr[0]=14;
+//     arr[1]=10;
+//     arr[2]=13;
+//     arr[3]=15;
+//     arr[4]=12;
+
+
+//     for(int i=0; i<arr.length;i++){
+//         System.out.println(arr[i]);
+//     }
+//    } 
+
+// }
+
 
 public class date_24_02 {
    public static void main(String[] args){
-    int[] arr = new int[5];
-    arr[0]=14;
-    arr[1]=10;
-    arr[2]=13;
-    arr[3]=15;
-    arr[4]=12;
-
-
-    for(int i=0; i<arr.length;i++){
-        System.out.println(arr[i]);
+    int[] vals={17,16,18,15,19,13,14};
+    for(int i =0;i<vals.length;i++){
+        System.out.println(vals[i]);
     }
-   } 
-
+   }
 }
