b5ca538997eb760287ac7929a10cf8834c506373
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-24 11:42:57 +0530

    store using for loop

--- MODULE_1/date_24_02.java
+++ MODULE_1/date_24_02.java
@@ -18,9 +18,20 @@
 // }
 
 
+
+
+// public class date_24_02 {
+//    public static void main(String[] args){
+//     int[] vals={17,16,18,15,19,13,14};
+//     for(int i =0;i<vals.length;i++){
+//         System.out.println(vals[i]);
+//     }
+//    }
+// }
+
 public class date_24_02 {
    public static void main(String[] args){
-    int[] vals={17,16,18,15,19,13,14};
+    double[] vals={17.4,16.0,18.99,15.77,19.99,13.45,14.75};
     for(int i =0;i<vals.length;i++){
         System.out.println(vals[i]);
     }
