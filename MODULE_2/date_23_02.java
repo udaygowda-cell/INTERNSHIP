7505545945d431d434060e9e2eca183e1578ef45
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-24 23:09:33 +0530

    static member accessing

--- MODULE_2/date_23_02.java
+++ MODULE_2/date_23_02.java
@@ -1,7 +1,19 @@
 //package MODULE_2;
 
-public class date_23_02 {
+class demo1{
+    static int x=100;
+    static int y =200;
+
+   static void test(){
+        System.out.println("hello word");
+    }
+}
+class date_23_02{
     public static void main(String[] args){
-        System.out.println("hello world");
+        System.out.println("main");
+        System.out.println("x "+demo1.x);
+        System.out.println("x "+demo1.y);
+        demo1.test();
+       System.out.println("end");
     }
 }
