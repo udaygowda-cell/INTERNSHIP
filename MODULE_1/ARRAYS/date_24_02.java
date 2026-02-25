cece2b5128d77f56bc4293038a6d119a61a22fc6
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-25 11:21:18 +0530

    find the element index and element are present in array

---
+++ MODULE_1/ARRAYS/date_24_02.java
@@ -0,0 +1,50 @@
+//package ARRAYS;
+//ARRAY
+
+// public class date_24_02 {
+//    public static void main(String[] args){
+//     int[] arr = new int[5];
+//     arr[0]=14;
+//     arr[1]=10;
+//     arr[2]=13;
+//     arr[3]=15;
+//     arr[4]=12;
+
+
+//     for(int i=0; i<arr.length;i++){
+//         System.out.println(arr[i]);
+//     }
+//    } 
+
+// }
+
+
+
+
+// public class date_24_02 {
+//    public static void main(String[] args){
+//     int[] vals={17,16,18,15,19,13,14};
+//     for(int i =0;i<vals.length;i++){
+//         System.out.println(vals[i]);
+//     }
+//    }
+// }
+
+// public class date_24_02 {
+//    public static void main(String[] args){
+//     double[] vals={17.4,16.0,18.99,15.77,19.99,13.45,14.75};
+//     for(int i =0;i<vals.length;i++){
+//         System.out.println(vals[i]);
+//     }
+//    }
+// }
+
+
+public class date_24_02 {
+   public static void main(String[] args){
+    char[] vals={'u','d','a','y'};
+    for(int i =0;i<vals.length;i++){
+        System.out.println(vals[i]);
+    }
+   }
+}
