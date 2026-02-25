8d059f9ec150bf5630c12430c8656180c4f9da73
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-25 10:50:53 +0530

    find sum of arrays

--- MODULE_1/date_25_02.java
+++ MODULE_1/date_25_02.java
@@ -14,15 +14,31 @@
 
 
 
+// public class date_25_02 {
+//     public static void main(String[] args){
+//         int[] arr={15,12,14,13,17,16};
+//         for(int i=arr.length-1;i>=0;i--){
+            
+//                 System.out.println(arr[i]);
+            
+          
+//         }
+//     }
+    
+// }
+
+
 public class date_25_02 {
     public static void main(String[] args){
-        int[] arr={15,12,14,13,17,16};
-        for(int i=arr.length-1;i>=0;i--){
-            
-                System.out.println(arr[i]);
+        int sum =0;
+        int[] arr={12,10,13,14,15};
+        for(int i=0;i<arr.length;i++)
+            {
+            sum =sum+arr[i];
+        }
+                System.out.println(sum);
             
           
         }
     }
     
-}
