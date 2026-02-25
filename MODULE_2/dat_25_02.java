5d5d275f1426e30149d3da5efa8ed97f801488b0
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-25 23:12:59 +0530

    merge

--- MODULE_2/dat_25_02.java
+++ MODULE_2/dat_25_02.java
@@ -39,6 +39,35 @@
 // 
 
 
+// class demo5 {
+//     int x=10;
+//     int y = 20;
+// }
+// class dat_25_02{
+//     public static void main(String[] args){
+//         System.out.println("main method start");
+//         demo5 d1 = new demo5();
+//         System.out.println(d1);
+//         System.out.println(d1.x);
+//         System.out.println(d1.y);
+//         System.out.println("after modification");
+
+//         d1.x=200;
+//         d1.y=600;
+//         demo5 d2 = new demo5();
+//          System.out.println(d2);
+
+//         System.out.println(d2.x);
+//         System.out.println(d2.y);
+
+//         System.out.println(d1.x);
+//         System.out.println(d1.y);
+       
+
+//     }
+// }
+
+
 class demo5 {
     int x=10;
     int y = 20;
@@ -48,14 +77,15 @@ class dat_25_02{
         System.out.println("main method start");
         demo5 d1 = new demo5();
         System.out.println(d1);
-        System.out.println(d1.x);
-        System.out.println(d1.y);
+        demo5 d2=d1;
+        System.out.println(d2);
+
         System.out.println("after modification");
 
         d1.x=200;
         d1.y=600;
-        demo5 d2 = new demo5();
-         System.out.println(d2);
+       
+   
 
         System.out.println(d2.x);
         System.out.println(d2.y);
