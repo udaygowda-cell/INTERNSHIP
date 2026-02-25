f7863bf1e90f2dae66899477ba8db2a56b41b0f9
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-25 10:41:47 +0530

    revers the array

--- MODULE_1/date_25_02.java
+++ MODULE_1/date_25_02.java
@@ -1,11 +1,26 @@
+// public class date_25_02 {
+//     public static void main(String[] args){
+//         int[] arr={15,12,14,13,17,16};
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]%2==0){//(arr[i]%2!=0)
+                
+//                 System.out.println("even number "+arr[i]);
+//             }
+          
+//         }
+//     }
+    
+// }
+
+
+
 public class date_25_02 {
     public static void main(String[] args){
         int[] arr={15,12,14,13,17,16};
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]%2==0){//(arr[i]%2!=0)
-                
-                System.out.println("even number "+arr[i]);
-            }
+        for(int i=arr.length-1;i>=0;i--){
+            
+                System.out.println(arr[i]);
+            
           
         }
     }
