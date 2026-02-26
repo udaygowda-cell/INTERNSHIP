5b082e20de9d938468a56e497bcc6e1f97a73560
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-26 19:40:55 +0530

    toCharArray()

--- date_26_02.java
+++ date_26_02.java
@@ -16,14 +16,25 @@
 
 //length()
 
+// public class date_26_02 {
+//     public static void main(String[] args){
+//         // String str = "developer";
+//         // System.out.println(str.length());
+
+//          String str = "Udaya kumar KJ";
+//         System.out.println(str.length());
+//     }
+//     }
+
+
+//toCharArray
+
 public class date_26_02 {
     public static void main(String[] args){
-        // String str = "developer";
-        // System.out.println(str.length());
-
-         String str = "Udaya kumar KJ";
-        System.out.println(str.length());
+        String str = "developer";
+       char[] ch = str.toCharArray();
+       for(int i=0;i<ch.length;i++){
+        System.out.println(ch[i]);
+       }
     }
     }
-
-
