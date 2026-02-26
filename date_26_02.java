5e2a5a20acdd2c51478be5b01341474680dc4bc7
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-26 19:30:23 +0530

    charAt()

---
+++ date_26_02.java
@@ -0,0 +1,15 @@
+//package STRING;
+//STRING
+
+//charAt()
+//
+public class date_26_02 {
+    public static void main(String[] args){
+        String str = "developer";
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(5));
+        System.out.println(str.charAt(1));
+        System.out.println(str.charAt(8));
+    }
+    }
+
