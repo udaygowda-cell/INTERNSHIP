bd6c99efa49d8b829b81ecd8070250e20530e0b0
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-26 19:35:49 +0530

    Length()

--- MODULE_1/STRING/date_26_02.java
+++ MODULE_1/STRING/date_26_02.java
@@ -2,15 +2,28 @@
 //STRING
 
 //charAt()
-//
+
+// public class date_26_02 {
+//     public static void main(String[] args){
+//         String str = "developer";
+//         System.out.println(str.charAt(0));
+//         System.out.println(str.charAt(5));
+//         System.out.println(str.charAt(1));
+//         System.out.println(str.charAt(8));
+//     }
+//     }
+
+
+//length()
+
 public class date_26_02 {
     public static void main(String[] args){
-        String str = "developer";
-        System.out.println(str.charAt(0));
-        System.out.println(str.charAt(5));
-        System.out.println(str.charAt(1));
-        System.out.println(str.charAt(8));
+        // String str = "developer";
+        // System.out.println(str.length());
+
+         String str = "Udaya kumar KJ";
+        System.out.println(str.length());
     }
     }
 
-hska
+
