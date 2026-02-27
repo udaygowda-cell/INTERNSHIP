3d279561d3cdba7159615ec08c38e5edc8ba66ae
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-27 23:32:50 +0530

    new

--- MODULE_1/STRING/date_27_02.java
+++ MODULE_1/STRING/date_27_02.java
@@ -18,16 +18,57 @@
 
 
 //trim
-public class date_27_02 {
-    public static void main(String[] args){
-        String str ="       core    java    ";
+// public class date_27_02 {
+//     public static void main(String[] args){
+//         String str ="       core    java    ";
 
-        System.out.println("---->"+str+"<----");
-        System.out.println("---->"+str.trim()+"<----");
+//         System.out.println("---->"+str+"<----");
+//         System.out.println("---->"+str.trim()+"<----");
    
 
       
 
+//     }
+// 
+
+
+//toUpperCase(), toLowercase()
+
+//  public class date_27_02 {
+//     public static void main(String[] args){
+//         String str = "Uday kumar K J";
+//         System.out.println(str.toUpperCase());
+//         System.out.println(str.toLowerCase());
+//     }
+// }
+
+
+//substring
+
+//  public class date_27_02 {
+//     public static void main(String[] args){
+//         String str = "Uday kumar K J";
+//         System.out.println(6);
+//         System.out.println(4);
+
+//         System.out.println(str.substring(3,8));
+//         System.out.println(str.substring(0,7));
+//     }
+// }
+
+//split
+
+ public class date_27_02 {
+    public static void main(String[] args){
+        String str = "qspider jspider pyspider prospider";
+        String[] arr=str.split(" ");
+       
+        for(int i=0;i<arr.length;i++){
+           System.out.println(arr[i]);
+
+        }
+       
+       
+        
     }
 }
-
