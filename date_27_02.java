8ba373be8f15502131a4b270b6f3623ad37dc8fb
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-27 11:28:05 +0530

    new

--- date_27_02.java
+++ date_27_02.java
@@ -1,5 +1,38 @@
-public class date_27_02 {
-    public static void main(String[] args){
+// //EQUALS(), EQUALSIGNORECASE()
+// public class date_27_02 {
+//     public static void main(String[] args){
+//         String str ="j2ee";
+
+//         System.out.println(str.equals("adv java"));
+//         System.out.println(str.equals("J2ee"));
+//         System.out.println(str.equals("j2ee"));
+
         
-    }
-}
+//         System.out.println(str.equalsIgnoreCase("adv java"));
+//         System.out.println(str.equalsIgnoreCase("J2ee"));
+//         System.out.println(str.equalsIgnoreCase("j2ee"));
+
+//     }
+// }
+
+
+
+//trim
+// public class date_27_02 {
+//     public static void main(String[] args){
+//         String str ="       core    java    ";
+
+//         System.out.println("---->"+str+"<----");
+//         System.out.println("---->"+str.trim()+"<----");
+   
+
+      
+
+//     }
+// }
+
+//new
+
+
+
+
