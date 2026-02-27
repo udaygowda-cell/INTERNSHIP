236efe8c00146c1f0392341fc62364f39b5260aa
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-27 15:41:48 +0530

    oops

---
+++ MODULE_2/class and object/dat_25_02.java
@@ -0,0 +1,98 @@
+// class demo5 {
+//     int x=10;
+//     int y = 20;
+// }
+// class dat_25_02{
+//     public static void main(String[] args){
+//         System.out.println("main method start");
+//         demo5 d1=new demo5();
+//         System.out.println(d1);
+//         demo5 d2 = new demo5();
+//         System.out.println(d2);
+//         demo5 d3 = new demo5();
+//         System.out.println(d3);
+//         //System.out.println(d1.x);
+//         //System.out.println(d1.y);
+
+//     }
+// }
+
+
+
+// class demo5 {
+//     int x=10;
+//     int y = 20;
+// }
+// class dat_25_02{
+//     public static void main(String[] args){
+//         System.out.println("main method start");
+//         demo5 d1 = new demo5();
+//         System.out.println(d1);
+//         System.out.println(d1.x);
+//         System.out.println(d1.y);
+       
+
+//     }
+// }
+
+
+// 
+
+
+// class demo5 {
+//     int x=10;
+//     int y = 20;
+// }
+// class dat_25_02{
+//     public static void main(String[] args){
+//         System.out.println("main method start");
+//         demo5 d1 = new demo5();
+//         System.out.println(d1);
+//         System.out.println(d1.x);
+//         System.out.println(d1.y);
+//         System.out.println("after modification");
+
+//         d1.x=200;
+//         d1.y=600;
+//         demo5 d2 = new demo5();
+//          System.out.println(d2);
+
+//         System.out.println(d2.x);
+//         System.out.println(d2.y);
+
+//         System.out.println(d1.x);
+//         System.out.println(d1.y);
+       
+
+//     }
+// }
+
+
+class demo5 {
+    int x=10;
+    int y = 20;
+}
+class dat_25_02{
+    public static void main(String[] args){
+        System.out.println("main method start");
+        demo5 d1 = new demo5();
+        System.out.println(d1);
+        demo5 d2=d1;
+        System.out.println(d2);
+
+        System.out.println("after modification");
+
+        d1.x=200;
+        d1.y=600;
+       
+   
+
+        System.out.println(d2.x);
+        System.out.println(d2.y);
+
+        System.out.println(d1.x);
+        System.out.println(d1.y);
+       
+
+    }
+}
---
+++ MODULE_2/class and object/date_27_02.java
@@ -0,0 +1,82 @@
+
+// class  Pen{
+//     String brand;
+//     String color;
+//     double price;
+// }
+// class date_27_02{
+//     public static void main(String[] args){
+//         System.out.println("main method started");
+//         Pen p1 =new Pen();
+//         p1.brand="classmate";
+//         p1.color="red";
+//         p1.price=22.0;
+//         System.out.println("brand "+p1.brand);
+//         System.out.println("color "+p1.color);
+//         System.out.println("price "+p1.price);
+
+// System.out.println("------------");
+//         Pen p2 =new Pen();
+//         p2.brand="doms";
+//         p2.color="green";
+//         p2.price=3.0;
+//         System.out.println("brand "+p2.brand);
+//         System.out.println("color "+p2.color);
+//         System.out.println("price "+p2.price);
+
+
+//     }
+    
+// }
+
+
+// class textbook{
+//     String title;
+//     String author;
+//     int pages;
+//     String std;
+//     double price;
+
+// }
+
+// class date_27_02{
+//     public static void main(String[] args){
+
+//         System.out.println("-----DETAILS OF TEXBOOK-------");
+//         System.out.println();
+//         textbook p1 = new textbook();
+//         p1.title="java programming";
+//         p1.author= "Naveen";
+//         p1.std= "Engineering";
+//         p1.pages=500;
+//         p1.price=4000;
+//         System.out.println("TITLE: "+p1.title);
+//         System.out.println("AUTHOR: "+p1.author);
+//         System.out.println("STD: "+p1.std);
+//         System.out.println("PAGES: "+p1.pages);
+//         System.out.println("PRICE: "+p1.price);
+
+//          System.out.println("------------");
+//          System.out.println();
+//          System.out.println("-------------");
+
+//         textbook p2 = new textbook();
+//         p2.title="KAMA";
+//         p2.author= "MADAN";
+//         p2.std= "18+";
+//         p2.pages=500;
+//         p2.price=3000;
+//         System.out.println("TITLE: "+p2.title);
+//         System.out.println("AUTHOR: "+p2.author);
+//         System.out.println("STD: "+p2.std);
+//         System.out.println("PAGES: "+p2.pages);
+//         System.out.println("PRICE: "+p2.price);
+
+
+
+//     }
+
+// }
+
+
+
