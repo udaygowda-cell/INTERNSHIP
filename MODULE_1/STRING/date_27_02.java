2a29510892b767b7c46491ca8d1a948de2e0f44c
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-27 11:16:21 +0530

    trim()

---
+++ MODULE_1/STRING/date_27_02.java
@@ -0,0 +1,33 @@
+// //EQUALS(), EQUALSIGNORECASE()
+// public class date_27_02 {
+//     public static void main(String[] args){
+//         String str ="j2ee";
+
+//         System.out.println(str.equals("adv java"));
+//         System.out.println(str.equals("J2ee"));
+//         System.out.println(str.equals("j2ee"));
+
+        
+//         System.out.println(str.equalsIgnoreCase("adv java"));
+//         System.out.println(str.equalsIgnoreCase("J2ee"));
+//         System.out.println(str.equalsIgnoreCase("j2ee"));
+
+//     }
+// }
+
+
+
+//trim
+public class date_27_02 {
+    public static void main(String[] args){
+        String str ="       core    java    ";
+
+        System.out.println("---->"+str+"<----");
+        System.out.println("---->"+str.trim()+"<----");
+   
+
+      
+
+    }
+}
+
