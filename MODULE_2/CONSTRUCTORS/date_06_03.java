6f278b1ba75644c1855b8514e951f92dab6c6572
udaygowda-cell <udaykjgowda@gmail.com>
2026-03-06 21:06:48 +0530

    static blocks

--- MODULE_2/CONSTRUCTORS/date_06_03.java
+++ MODULE_2/CONSTRUCTORS/date_06_03.java
@@ -14,7 +14,33 @@
 // }
 
 
+// class demo1{
+//     demo1(){
+//         System.out.println("constuctor  run");
+//     }
+//     {
+//         System.out.println("1st block programming");
+
+//     }
+//     {
+//         System.out.println("2nd block programming");
+//     }
+// }
+
+// public class date_06_03 {
+//     public static void main (String[] args){
+//         System.out.println("Main class ");
+//         new demo1();
+//         System.out.println("end main class");
+//     }
+// }
+
+
+
 class demo1{
+    static{
+        System.out.println("static block is used");
+    }
     demo1(){
         System.out.println("constuctor  run");
     }
