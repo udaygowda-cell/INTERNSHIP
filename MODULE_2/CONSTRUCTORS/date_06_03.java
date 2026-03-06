80405bc901255cb40dd86458c402eaa7500d5d16
udaygowda-cell <udaykjgowda@gmail.com>
2026-03-06 20:56:53 +0530

    constructor

---
+++ MODULE_2/CONSTRUCTORS/date_06_03.java
@@ -0,0 +1,14 @@
+//constuctor
+class demo1{
+    demo1(){
+        System.out.println("demo run");
+    }
+}
+
+public class date_06_03 {
+    public static void main (String[] args){
+        System.out.println("Main class ");
+        new demo1();
+        System.out.println("end main class");
+    }
+}
