9763855ed096516ceb1c664b5b045e271c1d1b11
udaygowda-cell <udaykjgowda@gmail.com>
2026-03-06 21:03:55 +0530

    nonstatic blocks

--- MODULE_2/CONSTRUCTORS/date_06_03.java
+++ MODULE_2/CONSTRUCTORS/date_06_03.java
@@ -1,7 +1,29 @@
 //constuctor
+// class demo1{
+//     demo1(){
+//         System.out.println("demo run");
+//     }
+// }
+
+// public class date_06_03 {
+//     public static void main (String[] args){
+//         System.out.println("Main class ");
+//         new demo1();
+//         System.out.println("end main class");
+//     }
+// }
+
+
 class demo1{
     demo1(){
-        System.out.println("demo run");
+        System.out.println("constuctor  run");
+    }
+    {
+        System.out.println("1st block programming");
+
+    }
+    {
+        System.out.println("2nd block programming");
     }
 }
 
