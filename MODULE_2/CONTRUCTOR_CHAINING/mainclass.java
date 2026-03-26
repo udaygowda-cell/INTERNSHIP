3d8d3e4567a56594552eb8e7e736def607a7c225
udaygowda-cell <udaykjgowda@gmail.com>
2026-03-26 06:48:22 +0530

    chain

--- MODULE_2/CONTRUCTOR_CHAINING/mainclass.java
+++ MODULE_2/CONTRUCTOR_CHAINING/mainclass.java
@@ -6,7 +6,7 @@ class Constuctor {
     String pwd;
     long cno;
     String email;
-    23456789012
+    234567890123
     public Constuctor(String userName, String pwd) {
         this.userName = userName;
         this.pwd = pwd;
