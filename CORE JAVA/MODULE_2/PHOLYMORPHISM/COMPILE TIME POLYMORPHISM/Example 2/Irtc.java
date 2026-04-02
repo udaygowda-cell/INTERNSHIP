703cf03c919b6b855999e47a8ef29c67db588a97
udaygowda-cell <udaykjgowda@gmail.com>
2026-04-02 16:11:08 +0530

    compile time polymorphism example 2

---
+++ CORE JAVA/MODULE_2/PHOLYMORPHISM/COMPILE TIME POLYMORPHISM/Example 2/Irtc.java
@@ -0,0 +1,17 @@
+package java.CORE JAVA.MODULE_2.PHOLYMORPHISM.COMPILE TIME POLYMORPHISM.Example 2;
+
+public class Irtc {
+
+    void search(String scr, String dest){
+        System.out.println("Route Details");
+    }
+
+    void search(int trainNumber){
+        System.out.println("Train Details");
+    }
+    void search(long pnr){
+        System.out.println("Tickets Details");
+    }
+    
+
+}
---
+++ CORE JAVA/MODULE_2/PHOLYMORPHISM/COMPILE TIME POLYMORPHISM/Example 2/Mainclass.java
@@ -0,0 +1,12 @@
+//package java.CORE JAVA.MODULE_2.PHOLYMORPHISM.COMPILE TIME POLYMORPHISM.Example 2;
+
+public class Mainclass {
+    public static void main(String[] args){
+        Irtc I = new Irtc();
+
+        I.search("Bangalore", "Delhi");
+        I.search(1231231);
+        I.search(1222132333L);
+
+    }
+}
