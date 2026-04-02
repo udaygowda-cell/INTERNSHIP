65977f0c781e4249fa09c4b911d742532dec1ba0
Udaya Kumar K J <udaykjgowda@gmail.com>
2026-04-02 23:01:14 +0530

    Update date_27_02.java

--- CORE JAVA/MODULE_1/STRING/date_27_02.java
+++ CORE JAVA/MODULE_1/STRING/date_27_02.java
@@ -2,7 +2,7 @@
 // public class date_27_02 {
 //     public static void main(String[] args){
 //         String str ="j2ee";
-12890122
+
 //         System.out.println(str.equals("adv java"));
 //         System.out.println(str.equals("J2ee"));
 //         System.out.println(str.equals("j2ee"));
