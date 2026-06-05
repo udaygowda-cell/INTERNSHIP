194bb2f65f8825f248af7de3c38d60f23159b77c
Udaya Kumar K J <udaykjgowda@gmail.com>
2026-06-05 23:40:05 +0530

    Update mostReapeat.java

--- PROGRAMMING/ARRAYS/mostReapeat.java
+++ PROGRAMMING/ARRAYS/mostReapeat.java
@@ -6,7 +6,7 @@ public class mostReapeat {
         int  mostrep=0;
         boolean b[]= new boolean[a.length];
 
-        for(int i = 0; i<=a.length-1;i++){
+         for(int i = 0; i<=a.length-1;i++){
             if(b[i]==false){
                 int count =1;
         for(int j = i+1; j<=a.length-1;j++){
