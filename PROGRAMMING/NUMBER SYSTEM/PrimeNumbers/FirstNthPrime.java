305ba28bdca07eddee1e612a65c8b1c926a330fb
Udaya Kumar K J <udaykjgowda@gmail.com>
2026-06-08 10:00:19 +0530

    Remove commented package declaration from FirstNthPrime

--- PROGRAMMING/NUMBER SYSTEM/PrimeNumbers/FirstNthPrime.java
+++ PROGRAMMING/NUMBER SYSTEM/PrimeNumbers/FirstNthPrime.java
@@ -2,7 +2,6 @@
 import java.util.Scanner;
 public class FirstNthPrime {
     //package java.PROGRAMMING;  
-
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
 
