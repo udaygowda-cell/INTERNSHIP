6b7fd59f7d225b19d50b6cd6a89e4c389040ad97
Udaya Kumar K J <udaykjgowda@gmail.com>
2026-06-08 10:01:32 +0530

    Update FirstNthPrime.java

--- PROGRAMMING/NUMBER SYSTEM/PrimeNumbers/FirstNthPrime.java
+++ PROGRAMMING/NUMBER SYSTEM/PrimeNumbers/FirstNthPrime.java
@@ -1,6 +1,6 @@
  //package PrimeNumbers;
 import java.util.Scanner;
-public class FirstNthPrime {
+public class FirstNthPrime { 
     //package java.PROGRAMMING; 
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
